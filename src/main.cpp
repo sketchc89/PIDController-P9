f19d2bc0eb84837822a9e569ee2ddc5d196e6a04
Chris Sketch <sketchc89@gmail.com>
2018-08-27 22:32:30 -0400

    Initialize PID

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,10 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-
+  double Kp = 0.2;
+  double Ki = 0.004;
+  double Kd = 3.0;
+  pid.Init(Kp, Ki, Kd);
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
