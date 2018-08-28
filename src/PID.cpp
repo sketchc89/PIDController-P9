2b45ab6114ab394b28abaa283aa6bf26f72d3647
Chris Sketch <sketchc89@gmail.com>
2018-08-27 22:26:59 -0400

    Basic PID equations

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+
+    PID::p_error = 0.0;
+    PID::i_error = 0.0;
+    PID::d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    PID::d_error = PID::p_error - cte;
+    PID::p_error = cte;
+    PID::i_error += cte;
 }
 
 double PID::TotalError() {
+    return -PID::p_error*PID::Kp - PID::i_error*PID::Ki - PID::d_error*Kd;
 }
 
